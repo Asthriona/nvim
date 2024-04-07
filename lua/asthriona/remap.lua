ef1ffd0a75f061d349d4ca49ebee2dd5eb6325ed
Makoto Kobayashi <im@asthriona.com>
2024-04-07 02:58:58 -0500

    Add remap.
    Add a remat that delete whatever is selected, and paste whatever is in the buffer.
    Without moving what was deleted to the buffer.

--- lua/asthriona/remap.lua
+++ lua/asthriona/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
+-- Go back home!
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Clear search!
 vim.keymap.set("n", "<leader>/", ':nohlsearch<CR>', { noremap = true, silent = true })
+-- Paste without loosing buffer!
+vim.keymap.set("x", "<leader>p", [["_dP]])
