b0a19ec379e81e551817d81a479139ca86f423d2
Makoto Kobayashi <im@asthriona.com>
2025-10-25 12:45:05 +0200

    Add Smear cursor for smooth cursoring

---
+++ lua/asthriona/plugins/smear-cursor.lua
@@ -0,0 +1,12 @@
+return { 
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        smear_between_buffers = true,
+        smear_between_neighbor_lines = true,
+        scroll_buffer_space = true,
+        stiffness = 0.3,
+        trailing_stiffness = 0.1,
+        trailing_xponent = 2,
+    gamma = 1
+    },
+}
